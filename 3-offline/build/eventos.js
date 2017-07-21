9112dc98f5f274b3f0917109aac597caf2eeb4ce
Carlos <carlosor@gmail.com>
2017-07-21 23:26:02 +0100

    curso base finalizado

--- 3-offline/build/eventos.js
+++ 3-offline/build/eventos.js
@@ -26,16 +26,39 @@ var Workout = React.createClass({
 			editing: false
 		};
 	},
-	handleLike: function () {
-		this.setState({
-			like: !this.state.like
-		});
+	edit: function () {
+		this.setState({ editing: true });
+	},
+	cancelEdit: function () {
+		this.setState({ editing: false });
+	},
+	save: function () {
+		this.props.onChange(this.refs.nuevoNombre.value, this.props.id);
+		this.setState({ editing: false });
 	},
 	remove: function () {
 		this.props.onRemove(this.props.id);
 		console.log(this.props.id);
 	},
-	render: function () {
+	showEditingView: function () {
+		return React.createElement(
+			"div",
+			{ className: "col-sm-6" },
+			React.createElement(
+				"div",
+				{ className: "workout-wrapper" },
+				React.createElement("input", {
+					type: "text",
+					ref: "nuevoNombre",
+					className: "form-control",
+					placeholder: "workout name",
+					defaultValue: this.props.workoutTitle }),
+				React.createElement("i", { className: "fa fa-times", "aria-hidden": "true", onClick: this.cancelEdit }),
+				React.createElement("i", { className: "fa fa-check", "aria-hidden": "true", onClick: this.save })
+			)
+		);
+	},
+	showFinalView: function () {
 		return React.createElement(
 			"div",
 			{ className: "col-sm-6" },
@@ -61,14 +84,6 @@ var Workout = React.createClass({
 				React.createElement(
 					"div",
 					null,
-					React.createElement("input", {
-						onChange: this.handleLike,
-						defaultChecked: this.state.like,
-						type: "checkbox", className: "fa fa-check" })
-				),
-				React.createElement(
-					"div",
-					null,
 					"Material: ",
 					String(this.state.like)
 				),
@@ -88,9 +103,17 @@ var Workout = React.createClass({
 					React.createElement(Circle, null),
 					React.createElement(Circle, null)
 				),
-				React.createElement("i", { className: "fa fa-times", "aria-hidden": "true", onClick: this.remove })
+				React.createElement("i", { className: "fa fa-times", "aria-hidden": "true", onClick: this.remove }),
+				React.createElement("i", { className: "fa fa-pencil", "aria-hidden": "true", onClick: this.edit })
 			)
 		);
+	},
+	render: function () {
+		if (this.state.editing) {
+			return this.showEditingView();
+		} else {
+			return this.showFinalView();
+		}
 	}
 });
 
@@ -102,29 +125,63 @@ var Workouts = React.createClass({
 			lista_workouts: ['Aphrodite', 'Zeus', 'Metis']
 		};
 	},
+	getDefaultProps() {
+		// cuando retornamos un objeto usamos llaves
+		return {
+			framework: 'React',
+			language: 'JavasScript'
+		};
+	},
+	componentWillMount: function () {
+		// esto se cargará antes de que se dibujen los componentes en pantalla
+		var pais;
+		var self = this;
+
+		$.getJSON('https://restcountries.eu/rest/v1/all', function (data) {
+			for (pais in data) {
+				console.log(data[pais].name);
+				self.addWorkout(data[pais].name);
+			}
+			$(self.refs.spinner).removeClass('fa-spin');
+			$(self.refs.spinner).hide();
+		});
+	},
+	componentDidMount() {
+		$(this.refs.spinner).addClass('fa-spin');
+	},
+	addWorkout: function (workoutTitle) {
+		var nuevoWorkout = this.refs.nuevoWorkout.value;
+		if (nuevoWorkout == '') {
+			if (typeof workoutTitle == 'undefined') {
+				nuevoWorkout = 'Nuevo workout';
+			} else {
+				nuevoWorkout = workoutTitle;
+			}
+		}
+		listaWorkouts = this.state.lista_workouts;
+		listaWorkouts.push(nuevoWorkout);
+		this.setState({ lista_workouts: listaWorkouts });
+		this.refs.nuevoWorkout.value = "";
+	},
 	removeWorkout: function (i) {
 		arr = this.state.lista_workouts;
 		arr.splice(i, 1); // eliminar índice del array
 		this.setState({ lista_workouts: arr });
 	},
+	updateWorkout: function (nuevoNombre, i) {
+		var arr = this.state.lista_workouts;
+		arr[i] = nuevoNombre;
+		this.setState({ lista_workouts: arr });
+	},
 	eachWorkout: function (workoutTitle, i) {
+		// usamos paréntesis después de return porque no es un objeto
 		return React.createElement(Workout, {
 			key: i,
 			id: i,
 			workoutTitle: workoutTitle,
 			dificultad: "standar",
-			onRemove: this.removeWorkout });
-	},
-	addWorkout: function () {
-		nuevoWorkout = this.refs.nuevoWorkout.value;
-		if (nuevoWorkout == '') {
-			listaWorkouts = this.state.lista_workouts;
-		} else {
-			listaWorkouts = this.state.lista_workouts;
-			listaWorkouts.push(nuevoWorkout);
-			this.setState({ lista_workouts: listaWorkouts });
-			this.refs.nuevoWorkout.value = "";
-		}
+			onRemove: this.removeWorkout,
+			onChange: this.updateWorkout });
 	},
 	handleKeyDown: function (e) {
 		if (e.charCode === 13) {
@@ -151,6 +208,16 @@ var Workouts = React.createClass({
 						{ className: "col-12" },
 						"N\xFAmero de workouts: ",
 						this.state.lista_workouts.length
+					),
+					React.createElement("i", { ref: "spinner", className: "fa fa-spinner", "aria-hidden": "true" }),
+					React.createElement(
+						"p",
+						null,
+						"Este ejercicio est\xE1 hecho con ",
+						this.props.framework,
+						", un framework de ",
+						this.props.language,
+						"."
 					)
 				),
 				React.createElement(
--- 3-offline/src/eventos.js
+++ 3-offline/src/eventos.js
@@ -24,17 +24,35 @@ var Workout = React.createClass({
         	editing: false
         }
     },
-	handleLike: function(){
-		this.setState({
-			like: !this.state.like
-		})
+	edit: function(){
+		this.setState({editing: true});
+	},
+	cancelEdit: function(){
+		this.setState({editing: false});
+	},
+	save: function(){
+		this.props.onChange(this.refs.nuevoNombre.value, this.props.id);
+		this.setState({editing: false});
 	},
 	remove: function(){
 		this.props.onRemove(this.props.id);
 		console.log(this.props.id);
 	},
 	showEditingView: function(){
-
+		return(
+				<div className="col-sm-6">
+					<div className="workout-wrapper">
+						<input 
+							type="text" 
+							ref="nuevoNombre" 
+							className="form-control" 
+							placeholder="workout name" 
+							defaultValue={this.props.workoutTitle} />
+						<i className="fa fa-times" aria-hidden="true" onClick={this.cancelEdit}></i>
+						<i className="fa fa-check" aria-hidden="true" onClick={this.save}></i>
+					</div>			
+				</div>
+		)
 	},
 	showFinalView: function(){
 		return(
@@ -42,12 +60,6 @@ var Workout = React.createClass({
 					<div className="workout-wrapper">
 						<h1>{this.props.workoutTitle}</h1> 
 						<p><i>({this.props.dificultad})</i></p>
-						<div>
-							<input 
-								onChange={this.handleLike}
-								defaultChecked={this.state.like}
-								type="checkbox" className="fa fa-check" />
-						</div>
 						<div>Material: {String(this.state.like)}</div>
 						<div>Dificultad: 
 							<Circle />
@@ -60,12 +72,18 @@ var Workout = React.createClass({
 							<Circle />
 						</div>
 						<i className="fa fa-times" aria-hidden="true" onClick={this.remove}></i>
+						<i className="fa fa-pencil" aria-hidden="true" onClick={this.edit}></i>
 					</div>
 				</div>
 			)
 	},
 	render: function(){
-		return.this.showFinalView;
+		if (this.state.editing) {
+			return this.showEditingView();
+		} else {
+			return this.showFinalView();
+		}
+		
 	}
 })
 
@@ -75,34 +93,66 @@ var Workouts = React.createClass({
 			lista_workouts: ['Aphrodite', 'Zeus', 'Metis']
 		}
 	},
+	getDefaultProps(){
+		// cuando retornamos un objeto usamos llaves
+		return {
+			framework: 'React',
+			language: 'JavasScript'
+		}
+	},
+	componentWillMount: function(){
+		// esto se cargará antes de que se dibujen los componentes en pantalla
+		var pais;
+		var self = this;
+
+		$.getJSON('https://restcountries.eu/rest/v1/all', function(data){
+			for(pais in data){
+				console.log(data[pais].name);
+				self.addWorkout(data[pais].name);
+			}
+			$(self.refs.spinner).removeClass('fa-spin');
+			$(self.refs.spinner).hide();
+		})
+	},
+	componentDidMount(){
+		$(this.refs.spinner).addClass('fa-spin');
+	},
+	addWorkout: function(workoutTitle){
+		var nuevoWorkout = this.refs.nuevoWorkout.value;
+		if (nuevoWorkout == '') {
+			if (typeof workoutTitle == 'undefined') {
+				nuevoWorkout = 'Nuevo workout';
+			} else {
+				nuevoWorkout =workoutTitle;
+			}
+		}
+		listaWorkouts = this.state.lista_workouts;
+		listaWorkouts.push(nuevoWorkout);
+		this.setState({lista_workouts: listaWorkouts});
+		this.refs.nuevoWorkout.value = "";	
+	},
 	removeWorkout: function(i){
 		arr = this.state.lista_workouts;
 		arr.splice(i, 1); // eliminar índice del array
 		this.setState({ lista_workouts: arr});
 	},
+	updateWorkout: function(nuevoNombre, i){
+		var arr = this.state.lista_workouts;
+		arr[i] = nuevoNombre;
+		this.setState({lista_workouts: arr});
+	},
 	eachWorkout: function(workoutTitle, i){
+		// usamos paréntesis después de return porque no es un objeto
 		return(
 			<Workout 
 				key={i}
 				id={i} 
 				workoutTitle={workoutTitle} 
 				dificultad="standar"
-				onRemove={this.removeWorkout} >
+				onRemove={this.removeWorkout} 
+				onChange={this.updateWorkout} >
 			</Workout>
 		)		
-	},
-	addWorkout: function(){
-		nuevoWorkout = this.refs.nuevoWorkout.value;
-		if (nuevoWorkout == '') {
-			listaWorkouts = this.state.lista_workouts;
-		} else {
-			listaWorkouts = this.state.lista_workouts;
-			listaWorkouts.push(nuevoWorkout);
-			this.setState({lista_workouts: listaWorkouts});
-			this.refs.nuevoWorkout.value = "";			
-		}
-		
-
 	},
 	handleKeyDown: function(e){
 		if (e.charCode === 13) {
@@ -116,6 +166,8 @@ var Workouts = React.createClass({
 					<div className="row">
 						<h1 className="col-12">Workouts</h1>
 						<p className="col-12">Número de workouts: {this.state.lista_workouts.length}</p>
+						<i ref="spinner" className="fa fa-spinner" aria-hidden="true"></i>
+						<p>Este ejercicio está hecho con {this.props.framework}, un framework de {this.props.language}.</p>
 					</div>
 					<div className="form-inline">
 						<input 
